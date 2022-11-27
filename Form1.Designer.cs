44dfd5dd560ee13b070de641d27b9da70b4d2e9f
grabe <grabe@LAPTOP-S7IOFPB5>
2022-11-27 20:54:37 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CKugelDlg
+{
+    partial class CKugelDlg
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtUmfang = new System.Windows.Forms.TextBox();
+            this.txtSchnittflaeche = new System.Windows.Forms.TextBox();
+            this.txtVolumen = new System.Windows.Forms.TextBox();
+            this.txtAnzahl = new System.Windows.Forms.TextBox();
+            this.btnBerechnen = new System.Windows.Forms.Button();
+            this.btnSchliessen = new System.Windows.Forms.Button();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblUmfang = new System.Windows.Forms.Label();
+            this.lblSchnittflaeche = new System.Windows.Forms.Label();
+            this.lblVolumen = new System.Windows.Forms.Label();
+            this.lblAnzahl = new System.Windows.Forms.Label();
+            this.m_intAnzahlBerechnungen = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.m_intAnzahlBerechnungen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(85, 10);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 0;
+            // 
+            // txtUmfang
+            // 
+            this.txtUmfang.Location = new System.Drawing.Point(85, 40);
+            this.txtUmfang.Name = "txtUmfang";
+            this.txtUmfang.ReadOnly = true;
+            this.txtUmfang.Size = new System.Drawing.Size(100, 20);
+            this.txtUmfang.TabIndex = 1;
+            // 
+            // txtSchnittflaeche
+            // 
+            this.txtSchnittflaeche.Location = new System.Drawing.Point(85, 70);
+            this.txtSchnittflaeche.Name = "txtSchnittflaeche";
+            this.txtSchnittflaeche.ReadOnly = true;
+            this.txtSchnittflaeche.Size = new System.Drawing.Size(100, 20);
+            this.txtSchnittflaeche.TabIndex = 2;
+            // 
+            // txtVolumen
+            // 
+            this.txtVolumen.Location = new System.Drawing.Point(85, 100);
+            this.txtVolumen.Name = "txtVolumen";
+            this.txtVolumen.ReadOnly = true;
+            this.txtVolumen.Size = new System.Drawing.Size(100, 20);
+            this.txtVolumen.TabIndex = 3;
+            // 
+            // txtAnzahl
+            // 
+            this.txtAnzahl.Location = new System.Drawing.Point(240, 70);
+            this.txtAnzahl.Name = "txtAnzahl";
+            this.txtAnzahl.ReadOnly = true;
+            this.txtAnzahl.Size = new System.Drawing.Size(50, 20);
+            this.txtAnzahl.TabIndex = 4;
+            // 
+            // btnBerechnen
+            // 
+            this.btnBerechnen.Location = new System.Drawing.Point(195, 10);
+            this.btnBerechnen.Name = "btnBerechnen";
+            this.btnBerechnen.Size = new System.Drawing.Size(95, 20);
+            this.btnBerechnen.TabIndex = 5;
+            this.btnBerechnen.Text = "Berechnen";
+            this.btnBerechnen.UseVisualStyleBackColor = true;
+            this.btnBerechnen.Click += new System.EventHandler(this.btnBerechnen_Click);
+            // 
+            // btnSchliessen
+            // 
+            this.btnSchliessen.Location = new System.Drawing.Point(195, 100);
+            this.btnSchliessen.Name = "btnSchliessen";
+            this.btnSchliessen.Size = new System.Drawing.Size(95, 20);
+            this.btnSchliessen.TabIndex = 6;
+            this.btnSchliessen.Text = "Schliessen";
+            this.btnSchliessen.UseVisualStyleBackColor = true;
+            this.btnSchliessen.Click += new System.EventHandler(this.btnSchliessen_Click);
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.Location = new System.Drawing.Point(10, 10);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(50, 20);
+            this.lblRadius.TabIndex = 7;
+            this.lblRadius.Text = "Radius";
+            // 
+            // lblUmfang
+            // 
+            this.lblUmfang.Location = new System.Drawing.Point(10, 40);
+            this.lblUmfang.Name = "lblUmfang";
+            this.lblUmfang.Size = new System.Drawing.Size(50, 20);
+            this.lblUmfang.TabIndex = 8;
+            this.lblUmfang.Text = "Umfang";
+            // 
+            // lblSchnittflaeche
+            // 
+            this.lblSchnittflaeche.Location = new System.Drawing.Point(10, 70);
+            this.lblSchnittflaeche.Name = "lblSchnittflaeche";
+            this.lblSchnittflaeche.Size = new System.Drawing.Size(75, 20);
+            this.lblSchnittflaeche.TabIndex = 9;
+            this.lblSchnittflaeche.Text = "Schnittfläche";
+            // 
+            // lblVolumen
+            // 
+            this.lblVolumen.Location = new System.Drawing.Point(10, 100);
+            this.lblVolumen.Name = "lblVolumen";
+            this.lblVolumen.Size = new System.Drawing.Size(75, 20);
+            this.lblVolumen.TabIndex = 10;
+            this.lblVolumen.Text = "Volumen";
+            // 
+            // lblAnzahl
+            // 
+            this.lblAnzahl.Location = new System.Drawing.Point(195, 70);
+            this.lblAnzahl.Name = "lblAnzahl";
+            this.lblAnzahl.Size = new System.Drawing.Size(45, 20);
+            this.lblAnzahl.TabIndex = 11;
+            this.lblAnzahl.Text = "Anzahl";
+            // 
+            // m_intAnzahlBerechnungen
+            // 
+            this.m_intAnzahlBerechnungen.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.m_intAnzahlBerechnungen.Location = new System.Drawing.Point(198, 37);
+            this.m_intAnzahlBerechnungen.Name = "m_intAnzahlBerechnungen";
+            this.m_intAnzahlBerechnungen.ReadOnly = true;
+            this.m_intAnzahlBerechnungen.Size = new System.Drawing.Size(84, 20);
+            this.m_intAnzahlBerechnungen.TabIndex = 12;
+            this.m_intAnzahlBerechnungen.Visible = false;
+            // 
+            // CKugelDlg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 126);
+            this.Controls.Add(this.m_intAnzahlBerechnungen);
+            this.Controls.Add(this.lblAnzahl);
+            this.Controls.Add(this.lblVolumen);
+            this.Controls.Add(this.lblSchnittflaeche);
+            this.Controls.Add(this.lblUmfang);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.btnSchliessen);
+            this.Controls.Add(this.btnBerechnen);
+            this.Controls.Add(this.txtAnzahl);
+            this.Controls.Add(this.txtVolumen);
+            this.Controls.Add(this.txtSchnittflaeche);
+            this.Controls.Add(this.txtUmfang);
+            this.Controls.Add(this.txtRadius);
+            this.Name = "CKugelDlg";
+            this.Text = "Kugel";
+            ((System.ComponentModel.ISupportInitialize)(this.m_intAnzahlBerechnungen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtUmfang;
+        private System.Windows.Forms.TextBox txtSchnittflaeche;
+        private System.Windows.Forms.TextBox txtVolumen;
+        private System.Windows.Forms.TextBox txtAnzahl;
+        private System.Windows.Forms.Button btnBerechnen;
+        private System.Windows.Forms.Button btnSchliessen;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblUmfang;
+        private System.Windows.Forms.Label lblSchnittflaeche;
+        private System.Windows.Forms.Label lblVolumen;
+        private System.Windows.Forms.Label lblAnzahl;
+        private System.Windows.Forms.NumericUpDown m_intAnzahlBerechnungen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CKugelDlg
+{
+    /// <summary>
+    /// Klasse CKugelDlg
+    /// Erbt von Klasse Form
+    /// </summary>
+    public partial class CKugelDlg : Form
+    {
+        /// <summary>
+        /// Konstruktor der Klasse CKugelDlg
+        /// </summary>
+        public CKugelDlg()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Schliesst das Fenster
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSchliessen_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Wollen Sie die Applikation wirklich schliessen?", "Schliessen", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                MessageBox.Show("Applikation wird geschlossen");
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Führt die Berechnung folgender Werte aus
+        /// 1.  Umfang
+        /// 2.  Schnittfläche
+        /// 3.  Volumen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBerechnen_Click(object sender, EventArgs e)
+        {
+            /*
+             *  Overflow Exceptions und allgemeine Exceptions abfangen 
+            */
+            try
+            {
+                //  Prüfen ob Usereingabe valid ist
+                (bool bValid, double dRadius) = CheckRadiusEingabe(txtRadius);
+
+                /*
+                 * Nur weitermachen, wenn die Usereingabe sauber ist.
+                 * Wenn valide, folgende Funktionen aufrufen:
+                 * 1.   Umfang berechnen        =   UmfangBerechnen
+                 * 2.   Schnittfläche berechnen =   SchnittflaecheBerechnen
+                 * 3.   Kugelvolumen berechnen  =   KugelVolumenBerechnen
+                 * 4.   Zähler erhöhen          =   AddCounter
+                */
+                if (bValid)
+                {
+                    double dPI = Math.PI;
+                    UmfangBerechnen(dRadius, dPI);
+                    SchnittflaecheBerechnen(dRadius, dPI);
+                    KugelVolumenBerechnen(dRadius, dPI);
+                    AddCounter(m_intAnzahlBerechnungen, txtAnzahl);
+                }
+            }
+            //  Overflows explizit Abfangen
+            catch(OverflowException ex)
+            {
+                string strMessage = "OverflowException: " + ex.ToString();
+                MessageBox.Show(strMessage, "Benutzereingabe", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+            //  Alle anderen Exceptions abfangen
+            catch(Exception ex)
+            {
+                string strMessage = "Exception: " + ex.ToString();
+                MessageBox.Show(strMessage, "Benutzereingabe", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        /// <summary>
+        /// Liest zuerst den Wert der Übergebeben TextBox aus.
+        /// Prüft dann ob es sich nicht um einen Alphanummerischen Wert oder einen NULL Wert handelt.
+        /// Gibt dann die validität sowie den Radius als Tupel zurück
+        /// </summary>
+        /// <param name="oTextBox"></param>
+        /// <returns></returns>
+        private (bool, double) CheckRadiusEingabe(TextBox oTextBox)
+        {
+            //  Variablen deklarieren und teils definieren
+            string strRadius = oTextBox.Text;                                       //  Radius aus Textfeld holen
+            double dRadius;
+            bool bRadius_Is_NULL = string.IsNullOrEmpty(strRadius);                 //  Prüfen ob Eingabe NULL ist
+            bool bRadius_Is_Parsable = double.TryParse(strRadius, out dRadius);     //  Prüfen ob Eingabe geparsed werden kann und wenn ja => Parsen
+
+            //  Validität Standardmässig false
+            bool bValid = false;
+
+            /*
+             * Prüfen ob Radius NULL ist oder nicht geparsed werden konnte
+             * Fehler werfen wenn beides nicht zutrifft
+            */
+            if(!bRadius_Is_NULL && bRadius_Is_Parsable)     //  Normalfall
+            {
+                bValid = true;
+            }
+            else                                            //  Ausnahme
+            {
+                string strMessage = $"Die Eingabe des Radius ist nicht korrekt: '{strRadius}'";
+                ThrowUserExecption(strMessage, oTextBox);
+                bValid = false;
+            }
+
+            //  Zurückgeben der validität und des Radius als Double
+            return (bValid, dRadius);
+        }
+
+        /// <summary>
+        /// Berechnet den Umfang und übergibt den Wert ins Umfangsfeld
+        /// </summary>
+        /// <param name="dRadius"></param>
+        /// <param name="dPI"></param>
+        private void UmfangBerechnen(double dRadius, double dPI)
+        {
+            double dUmfang_U = 2 * dRadius * dPI;
+            AddValue(dUmfang_U, txtUmfang);
+        }
+
+        /// <summary>
+        /// Berechnet die Schnittfläche der Kugel (wie Kreis) und übergibt diesem dem Schnittflächenfeld
+        /// </summary>
+        /// <param name="dRadius"></param>
+        /// <param name="dPI"></param>
+        private void SchnittflaecheBerechnen(double dRadius, double dPI)
+        {
+            double dSchnittflaeche_F = Math.Pow(dRadius, 2.0) * dPI;
+            AddValue(dSchnittflaeche_F, txtSchnittflaeche);
+        }
+
+        /// <summary>
+        /// Berechnet das Volumen der Kugel und übergibt den Wert ans Kugelvolumenfeld
+        /// </summary>
+        /// <param name="dRadius"></param>
+        /// <param name="dPI"></param>
+        private void KugelVolumenBerechnen(double dRadius, double dPI)
+        {
+            double dKugelvolumen_V = 4.0 / 3.0 * Math.Pow(dRadius, 3.0) * dPI;
+            AddValue(dKugelvolumen_V, txtVolumen);
+        }
+
+        /// <summary>
+        /// Gibt den Übergebenen Fehlertext einer MessageBox,
+        /// selektiert den ganzen Text der übergebenen TextBox und legt den Fokus auf die übergebene TextBox
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="oTextBox"></param>
+        private void ThrowUserExecption(string strMessage, TextBox oTextBox)
+        {
+            MessageBox.Show(strMessage, "Benutzereingabe", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            oTextBox.SelectAll();
+            oTextBox.Focus();
+        }
+
+        /// <summary>
+        /// Setzt den übergebenen Wert an die übergebene TextBox
+        /// </summary>
+        /// <param name="dWert"></param>
+        /// <param name="oTextBox"></param>
+        private void AddValue(double dWert, TextBox oTextBox)
+        {
+            oTextBox.Text = dWert.ToString();
+        }
+
+        /// <summary>
+        /// Imkrementiert die Anzahl berechnungen
+        /// </summary>
+        /// <param name="oNumDown"></param>
+        /// <param name="oTextBox"></param>
+        private void AddCounter(NumericUpDown oNumDown, TextBox oTextBox)
+        {
+            /*
+             *  Integer (NummericDown) Feld
+            */
+            //  Auslesen des Wertes
+            int nAnzahlElemente = Convert.ToInt32(oNumDown.Value);
+
+            //  Inkrementieren
+            nAnzahlElemente++;
+
+            // Zurückschreiben
+            oNumDown.Value = nAnzahlElemente;
+
+            /*
+             * Textfeld
+            */
+            oTextBox.Text = nAnzahlElemente.ToString();
+        }
+
+    }
+}
